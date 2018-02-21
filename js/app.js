9a239235b2b86dcee60d48ae4a0ac1de7da26bba
IamCharlesM <because@charlesis.me>
2018-02-21 14:54:57 -0600

    Dock is centered and no longer moves with window adjustment.

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ $(".close-window").on('click',closeThis)
 
 
 
+
+
 //<!-- THIS IS FOR THE COOL DRAGGING AND RESIZING EFFECT -->
 // target elements with the "draggable" class
 interact('.draggable')
@@ -31,20 +33,10 @@ interact('.draggable')
       elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
     },
     // enable autoScroll
-    autoScroll: true,
+    autoScroll: false,
 
     // call this function on every dragmove event
     onmove: dragMoveListener,
-    // call this function on every dragend event
-//    onend: function (event) {
-//      var textEl = event.target.querySelector('p');
-//
-//      textEl && (textEl.textContent =
-//        'moved a distance of '
-//        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-//                     Math.pow(event.pageY - event.y0, 2) | 0))
-//            .toFixed(2) + 'px');
-//    }
   });
 
   function dragMoveListener (event) {
@@ -63,7 +55,7 @@ interact('.draggable')
     target.setAttribute('data-y', y);
   }
 
-  // this is used later in the resizing and gesture demos
+
   window.dragMoveListener = dragMoveListener;
 
 //===========================
